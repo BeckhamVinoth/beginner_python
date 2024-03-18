82808e3c2038cca3abf92199e25f87ff1dc451c7
BeckhamVinoth <vinoth.10decoders@gmail.com>
2024-03-18 18:08:57 +0530

    Empty check added for new todos

--- web.py
+++ web.py
@@ -1,15 +1,28 @@
+import streamlit
 import streamlit as sl
 import functions
 
-
 todos = functions.get_todos()
+
+
+def add_new():
+    if sl.session_state['new_todo'].strip():
+        new_todo = sl.session_state['new_todo'] + "\n"
+        todos.append(new_todo)
+        functions.write_todos(todos)
+
+
 sl.title('My ToDo App')
 sl.subheader('We can add and complete ur day today items here')
 
 for todo in todos:
     sl.checkbox(todo)
 
-sl.text_input(label='',placeholder='Type in new todo..')
+sl.text_input(label='input',
+              label_visibility='hidden',
+              placeholder='Type in new todo..',
+              key='new_todo',
+              on_change=add_new)
 
 
-
+sl.session_state
